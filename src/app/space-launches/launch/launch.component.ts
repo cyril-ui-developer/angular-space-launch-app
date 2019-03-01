75189639590b6ba2279237552cea3bf7c626f2da
Cyril Ajieh <>
2019-03-01 13:42:08 -0500

    refactored launches component grid

--- src/app/space-launches/launch/launch.component.ts
+++ src/app/space-launches/launch/launch.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ILaunch } from '../launch';
 
 @Component({
-  selector: 'app-lunch',
+  selector: '[app-launch]',
   templateUrl: './launch.component.html',
   styleUrls: ['./launch.component.scss']
 })
 export class LaunchComponent implements OnInit {
 
+  @Input() launch: ILaunch;
+  @Input() columns: string[];
   constructor() { }
 
   ngOnInit() {
--- src/app/space-launches/launches/launches.component.ts
+++ src/app/space-launches/launches/launches.component.ts
@@ -8,13 +8,21 @@ import { ILaunch } from '../launch';
   styleUrls: ['./launches.component.scss']
 })
 export class LaunchesComponent implements OnInit {
-lunches: ILaunch[];
+  lunches: ILaunch[];
+  columnTitles: string[] = ['Flight Number', 'Launch Year', 'Rocket Name', 'Details'];
+  columns: string[] = ['flightNumber', 'launchYear', 'rocketName', 'details'];
+  // columns = [
+  //   { flightNumber: 'Flight Number' },
+  //   { launchYear: 'Launch Year'},
+  //   { rocketName: 'Rocket Name' },
+  //   { details: 'Details' }
+  // ];
 
   constructor(private lunchesService: LunchesService) { }
 
   ngOnInit() {
-    this.lunchesService.getAllLunches()
-    .subscribe((data) => this.lunches = data);
+    this.lunchesService
+      .getAllLunches()
+      .subscribe(data => (this.lunches = data));
   }
-
 }
