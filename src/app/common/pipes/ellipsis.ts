d8f84d868e48e7de42f12f3a63bf1e5fb72de608
Cyril Ajieh <>
2019-03-01 11:10:43 -0500

    Custom truncate text pipe

---
+++ src/app/common/pipes/ellipsis.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'ellipsis'
+})
+export class EllipsisPipe implements PipeTransform{
+  transform( value: string, args: number) {
+    if (args === undefined || args === null) {
+      return value;
+    } else if (value === undefined || value === null){
+       return 'N/A';
+    } else if (value.length > args) {
+      return value.substring(0, args) + '...';
+    } else {
+      return value;
+    }
+  }
+}
